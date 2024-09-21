c517a3e2b44047f48c124e1a8e9cecab8aa34d44
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-21 21:29:10 +0300

    task 6.2.2

--- SkillFactory6OOP/Program.cs
+++ SkillFactory6OOP/Program.cs
@@ -2,6 +2,24 @@
 
 class Program
 {
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen()
+        {
+            color = "Чёрный";
+            cost = 100;
+        }
+
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+        }
+    }
+
 
     public static void Main(string[] args)
     {
