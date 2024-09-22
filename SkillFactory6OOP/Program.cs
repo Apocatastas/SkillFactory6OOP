1c1bea708a7c8fbc16170983b9dd1132359d5ac1
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-22 13:03:14 +0300

    task 6.5.2

--- SkillFactory6OOP/Program.cs
+++ SkillFactory6OOP/Program.cs
@@ -2,22 +2,66 @@
 
 class Program
 {
-    class Bus
-    {
-        public int? Load;
+    В классах нужно объявить поля для длин сторон(радиус) и
+        методы для получения площади и периметра(длины окружности)
+        без их реализации и без конструкторов.
 
-        public void PrintStatus()
+    class Square
+    {
+        int side;
+
+        double Area()
         {
-            if ((Load.HasValue)&&(Load > 0))
-            {
-                Console.WriteLine("В автобусе {0} пассажиров", Load.Value);
-            }
-            else
-            {
-                Console.WriteLine("Автобус пуст");
-            }
+            return side * side;
+        }
+
+        int Perimeter()
+        {
+            return side * 4;
         }
     }
+
+    class Triangle
+    {
+        int sideA;
+        int sideB;
+        int sideC;
+
+        int Perimeter()
+        {
+            return sideA + sideB + sideC;
+        }
+
+        double HeronSideProcessor(int side)
+        {
+            return Perimeter() / 2 - side;
+        }
+
+        double Area()
+        {
+            return Math.Sqrt( Perimeter() / 2
+                              * HeronSideProcessor(sideA)
+                              * HeronSideProcessor(sideB)
+                              * HeronSideProcessor(sideC)
+                             );
+        }
+    }
+
+    class Circle
+    {
+        int radius;
+
+        double circleLength()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        double circleArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
 
     public static void Main(string[] args)
     {
