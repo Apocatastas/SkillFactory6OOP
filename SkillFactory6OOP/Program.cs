a1132174609ffc98be5875cac06ed127bdaee527
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-22 13:11:38 +0300

    task 6.6.1

--- SkillFactory6OOP/Program.cs
+++ SkillFactory6OOP/Program.cs
@@ -3,59 +3,18 @@
 class Program
 {
 
-    class Square
+    class TrafficLight
     {
-        public int side;
+        string color;
 
-        public double Area()
+        private void ChangeColor(string color)
         {
-            return side * side;
+
         }
 
-        public int Perimeter()
+        public string GetColor()
         {
-            return side * 4;
-        }
-    }
-
-    class Triangle
-    {
-        public int sideA;
-        public int sideB;
-        public int sideC;
-
-        public int Perimeter()
-        {
-            return sideA + sideB + sideC;
-        }
-
-        public double HeronSideProcessor(int side)
-        {
-            return Perimeter() / 2 - side;
-        }
-
-        public double Area()
-        {
-            return Math.Sqrt( Perimeter() / 2
-                              * HeronSideProcessor(sideA)
-                              * HeronSideProcessor(sideB)
-                              * HeronSideProcessor(sideC)
-                             );
-        }
-    }
-
-    class Circle
-    {
-        public int radius;
-
-        public double circleLength()
-        {
-            return 2 * Math.PI * radius;
-        }
-
-        public double circleArea()
-        {
-            return Math.PI * radius * radius;
+            return color;
         }
     }
 
