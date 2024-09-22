b0251897c88c79b29dd0b5163ad760a946e14606
ɐllɐ <molchanova.a.a@gmail.com>
2024-09-22 13:23:41 +0300

    task 6.6.2

--- SkillFactory6OOP/Program.cs
+++ SkillFactory6OOP/Program.cs
@@ -3,18 +3,68 @@
 class Program
 {
 
-    class TrafficLight
+    class User
     {
-        string color;
+        private int age;
+        private string login;
+        private string email;
 
-        private void ChangeColor(string color)
+        public int Age
         {
+            get
+            {
+                return age;
+            }
 
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Возраст должен быть не меньше 18");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
         }
 
-        public string GetColor()
+        public string Login
         {
-            return color;
+            get
+            {
+                return login;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    Console.WriteLine("Логин должен быть минимум 3 символа");
+                }
+                else
+                {
+                    login = value;
+                }
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                if (!value.Contains('@'))
+                {
+                    Console.WriteLine("Адрес email должен содержать знак @");
+                }
+                else
+                {
+                    login = value;
+                }
+            }
         }
     }
 
